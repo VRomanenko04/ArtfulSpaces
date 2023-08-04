76f9494adfc38c6bbc4ba849ef8e47a7a9865e0b
VRomanenko04 <vovasan2004@gmail.com>
2023-08-05 02:56:49 +0300

    Сделал блок pricing одним simple компонентом

---
+++ src/components/simple/pricing-block/PricingBlock.tsx
@@ -0,0 +1,24 @@
+import classes from './_pricing-block.module.scss';
+import Button from '../../UI/button/Button';
+
+const PricingBlock = () => {
+    return (
+        <section className={classes.pricing__container}>
+            <h4>Pricing example</h4>
+            <div className={classes.pricing__block}>
+                <div className={classes.up__side}>
+                    <p>
+                        The price depends directly on your wishes, the footage of the room,
+                        the detail of the work, the selection of furniture and edits.
+                    </p>
+                </div>
+                <div className={classes.down__side}>
+                    <p>The site has an approximate price calculator, you can use it, after which you will contact us free of charge for the exact price.</p>
+                    <Button text='Price calculator' link='calculator' extraClass={classes.button}/>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default PricingBlock
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import Header from '../../components/ordinary/home-header/Header';
 import HomeInfo from '../../components/simple/home-info/HomeInfo';
+import PricingBlock from '../../components/simple/pricing-block/PricingBlock';
 import SliderBlock from '../../components/simple/slider-block/SliderBlock';
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
         <Header/>
         <HomeInfo/>
         <SliderBlock/>
+        <PricingBlock/>
         </>
     )
 }
