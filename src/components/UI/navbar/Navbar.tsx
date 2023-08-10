acedc130c1936ec4b67d2e6157e9c781072559b5
VRomanenko04 <vovasan2004@gmail.com>
2023-08-10 22:55:09 +0300

    Сделал смену класса Navbar зависимо от URL

--- src/components/UI/navbar/Navbar.tsx
+++ src/components/UI/navbar/Navbar.tsx
@@ -9,22 +9,32 @@ import logo from '../../../assets/ArtfulSpaces_logo.png';
 import cabinet_btn from '../../../assets/portrait.png';
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState<boolean>(false);
     const [hideOrShow, setHideOrShow] = useState({});
+    const [componentClass, setComponentClass] = useState<string>('menu_link');
     const location = useLocation();
 
-        //Смена состояния и стиля элемента
-        const handleMenu = () => {
-            setIsOpen((prev) => !prev);
-            setHideOrShow(() => {
-                if (isOpen) {
-                    return {};
-                } else {
-                    return { display: 'flex'};
-                }
-            });
-        }
+    //Смена состояния и стиля элемента
+    const handleMenu = () => {
+        setIsOpen((prev) => !prev);
+        setHideOrShow(() => {
+            if (isOpen) {
+                return {};
+            } else {
+                return { display: 'flex'};
+            }
+        });
+    }
 
+    //Смена класса по URL
+    const isHomePage: boolean = location.pathname === '/';
+    const newClass: string = isHomePage ? 'menu_link_home' : 'menu_link';
+
+    if (componentClass !== newClass) {
+        setComponentClass(newClass);
+    }
+
+    //Закрытие после перехода по страницам
     useEffect(() => {
         setIsOpen(false);
     }, [location.pathname]);
@@ -47,10 +57,10 @@ const Navbar = () => {
                                             animate={{ x: isOpen ? 0 : (matches.small ? 500 : 0)}}
                                             transition={{ duration: 0.8 }}
                                         >
-                                            <NavLink className='menu_link' to='/'>Home</NavLink>
-                                            <NavLink className='menu_link' to='order'>Order</NavLink>
-                                            <NavLink className='menu_link' to='examples'>Work Examples</NavLink>
-                                            <NavLink className='menu_link' to='contact'>Contact</NavLink>
+                                            <NavLink className={componentClass} to='/'>Home</NavLink>
+                                            <NavLink className={componentClass} to='order'>Order</NavLink>
+                                            <NavLink className={componentClass} to='examples'>Work Examples</NavLink>
+                                            <NavLink className={componentClass} to='contact'>Contact</NavLink>
                                             {location.pathname !== 'cabinet' && (
                                                 <motion.li className='nav__li' >
                                                     <Link className='cabinet' to='cabinet'>
