711339d86e6ee1b305b3a6d5343e4756ce4130eb
VRomanenko04 <vovasan2004@gmail.com>
2023-08-12 02:27:13 +0300

    Сделал Header страницы Order

---
+++ src/components/ordinary/order-header/OrderHeader.tsx
@@ -0,0 +1,15 @@
+import classes from './_order-header.module.scss';
+
+const OrderHeader = () => {
+    return (
+        <div>
+            <div className={classes.container}>
+                <h1>
+                    Order a unique 3D interior design: Turn your ideas into reality!
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default OrderHeader
--- src/pages/order/Order.tsx
+++ src/pages/order/Order.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import OrderHeader from '../../components/ordinary/order-header/OrderHeader';
 
 
 const Order = () => {
     return (
-        <div>Order</div>
+        <>
+        <OrderHeader/>
+        </>
     )
 }
 
