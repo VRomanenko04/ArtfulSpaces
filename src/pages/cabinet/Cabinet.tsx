2dfe8edb06fc2fcd8b2ed6e6602b67704bce46f4
VRomanenko04 <vovasan2004@gmail.com>
2023-08-30 19:34:41 +0300

    Сделал форму логина для проверки заказов

--- src/pages/cabinet/Cabinet.tsx
+++ src/pages/cabinet/Cabinet.tsx
@@ -1,16 +1,88 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Popup from '../../components/simple/popup/Popup';
+import BasicInput from '../../components/UI/basic-input/BasicInput';
+import classes from './_cabinet.module.scss';
 
 
 const Cabinet = () => {
     const [modalActive, setModalActive] = useState(false);
 
+    const [loginData, setLoginData] = useState(() => {
+        const storedLoginData = JSON.parse(sessionStorage.getItem('loginData') || '{}');
+        return {
+            email: storedLoginData.email || '',
+            phoneNumber: storedLoginData.phoneNumber || ''
+        };
+    });
+
+    const [formData, setFormData] = useState({
+        email: '',
+        phoneNumber: '',
+        firstName: '',
+        lastName: ''
+    });
+
+    useEffect(() => {
+        const storedOrderData = JSON.parse(localStorage.getItem('formData') || '{}');
+        setFormData(storedOrderData);
+    }, []);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setLoginData((prevData) => ({
+            ...prevData,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => { // Указываем тип FormEvent
+        event.preventDefault();
+        setModalActive(true);
+        sessionStorage.setItem('loginData', JSON.stringify(loginData));
+    };
+
+    const shouldShowPopup = (loginData.email === formData.email && loginData.phoneNumber === formData.phoneNumber) &&
+    loginData.email !== '' && loginData.phoneNumber !== '';
+
     return (
-        <section>
-            <button onClick={() => setModalActive(true)}>Orders</button>
-            <Popup active={modalActive} setActive={setModalActive}>
-                <p>Hello</p>
-            </Popup>
+        <section className={classes.container}>
+            <h2>Check your order status</h2>
+            <form className={classes.form} onSubmit={handleSubmit}>
+                <BasicInput
+                    labelText='E-mail:'
+                    htmlFor='email'
+                    type='email'
+                    id='email'
+                    name='email'
+                    placeholder='your@gmail.com'
+                    value={loginData.email}
+                    onChange={handleInputChange}
+                />
+                <BasicInput
+                    labelText='Phone Number:'
+                    htmlFor="phoneNumber"
+                    type="tel"
+                    id="phoneNumber"
+                    name="phoneNumber"
+                    placeholder='380556667788'
+                    value={loginData.phoneNumber}
+                    onChange={handleInputChange}
+                />
+                <button className={classes.btn}>Check order status</button>
+            </form>
+            {shouldShowPopup ? 
+                <Popup active={modalActive} setActive={setModalActive}>
+                    <p>
+                        Hello, {formData.firstName} {formData.lastName}!
+                    </p>
+                </Popup>
+                :
+                <Popup active={modalActive} setActive={setModalActive}>
+                    <p>
+                        You don't have order yet ):
+                    </p>
+                </Popup>
+            }
         </section>
     )
 }
