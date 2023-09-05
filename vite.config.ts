0c793749660d2c864c9f347d4463775ac474f40a
VRomanenko04 <vovasan2004@gmail.com>
2023-09-06 01:46:02 +0300

    6 попытка deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ArtfulSpaces/",
   plugins: [react()],
-  base: "/ArtfulSpaces/"
 })
