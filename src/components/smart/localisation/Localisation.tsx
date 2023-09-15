15c765b5af89538f331f68b7661fae3a8cf5145b
VRomanenko04 <vovasan2004@gmail.com>
2023-09-16 01:03:07 +0300

    Оформил компонент Localisation и убрал уязвимости

--- src/components/smart/localisation/Localisation.tsx
+++ src/components/smart/localisation/Localisation.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import classes from './_localisation.module.scss';
 import { useTranslation } from 'react-i18next';
 
 
 const Localisation: React.FC = () => {
-    const { i18n } = useTranslation()
+    const { i18n } = useTranslation();
+    const [selectedLanguage, setSelectedLanguage] = useState<string>('');
 
     const changeLanguage = (lng: string) => {
         i18n.changeLanguage(lng);
+        setSelectedLanguage(lng);
+        sessionStorage.setItem('selectedLanguage', lng);
     }
 
+    const handleLanguageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const selectedLanguage = event.target.value;
+        changeLanguage(selectedLanguage);
+    }
+
+    useEffect(() => {
+        const savedLanguage = sessionStorage.getItem('selectedLanguage');
+        if (savedLanguage) {
+            setSelectedLanguage(savedLanguage); 
+            changeLanguage(savedLanguage); 
+        }
+    }, [])
+
     return (
-        <div>
-            <button onClick={() => changeLanguage('en')}>EN</button>
-            <button onClick={() => changeLanguage('ua')}>UA</button>
-        </div>
+        <select className={classes.selector} onChange={handleLanguageChange} value={selectedLanguage}>
+            <option value="en">EN</option>
+            <option value="ua">UA</option>
+        </select>
     )
 }
 
