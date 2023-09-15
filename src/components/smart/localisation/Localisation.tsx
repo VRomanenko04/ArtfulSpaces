8fc550f524398ed021cb1482f7a401c5b606f380
VRomanenko04 <vovasan2004@gmail.com>
2023-09-15 22:41:53 +0300

    Создал компонент-заготовку под лаколизацию

---
+++ src/components/smart/localisation/Localisation.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './_localisation.module.scss';
+
+
+const Localisation: React.FC = () => {
+    return (
+        <div>Localisation</div>
+    )
+}
+
+export default Localisation;
